dbde5072bf00cbb29f1dd9bc3ba616326f3e8e79
Shyamolina <ghoshshyamolina89@gmail.com>
2022-05-04 21:22:43 +0200

    edits with 15 sp threshold

--- R/for_RivFishTIME/RivFishTIME_ta.R
+++ R/for_RivFishTIME/RivFishTIME_ta.R
@@ -58,6 +58,54 @@ for(i in 1:length(good_TimeSeriesID_q3q4)){
   cat("---------- i= ",i," siteid=",siteid," ----------\n")
 }
 
+#--------------------------------------------------------------------
+# compute between years and between species spearman correlations (only significant)
+# for each input year by species matrix
+
+source("compute_avg_cor.R") # minimum 5 species, minimum 5 years 
+# are required to estimate this metric, we are considering always >=20 years, and >=15 species 
+# so it's okay for years
+for(i in c(1:length(good_TimeSeriesID_q3q4))){
+  
+  siteid<-good_TimeSeriesID_q3q4[i]
+  resloc_output<-paste(resloc,siteid,"/",sep="")
+  
+  resloc_input<-paste("../../DATA/for_RivFISHTIME/wrangled_data/",siteid,"/",sep="")
+  df<-readRDS(paste(resloc_input,"commonspecies_timeseries.RDS",sep="")) # dataframe with species timeseries along column
+  
+  id<-which(colnames(df)=="raresp")
+  if(length(id)>0){
+    df<-df[,-id]
+  }
+  
+  res<-compute_avg_cor(mat=df)
+  saveRDS(res,paste(resloc_output,"avg_cor_metric.RDS",sep=""))
+  
+}
+
+#--------------- Do a summary stats  ------------------
+summary_table<-c()
+for (i in c(1:length(good_TimeSeriesID_q3q4))){
+  siteid<-good_TimeSeriesID_q3q4[i]
+  resloc_input<-paste(resloc,siteid,"/",sep="")
+  x<-readRDS(paste(resloc_input,"summary_df.RDS",sep=""))
+  y<-readRDS(paste(resloc_input,"avg_cor_metric.RDS",sep=""))
+  z<-cbind(x,y)
+  summary_table<-rbind(summary_table,z)
+}
+summary_table<-cbind(siteid=good_TimeSeriesID_q3q4,summary_table)
+
+# to get initial richness
+for(i in 1:nrow(summary_table)){
+  siteid<-summary_table$siteid[i]
+  resloc_input<-paste("../../DATA/for_RivFishTIME/wrangled_data/",siteid,"/",sep="")
+  bigM<-readRDS(paste(resloc_input,"allspecies_timeseries.RDS",sep=""))
+  summary_table$initR[i]<-ncol(bigM)
+}
+# reorganize
+summary_table<-summary_table%>%dplyr::select(siteid,initR,nsp,nyr,nint,nind,npos,nL,nU,nneg,L,U)
+
+saveRDS(summary_table,"../../Results/for_RivFishTIME/summary_table.RDS")
 
 
 
